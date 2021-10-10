18f6ca2add4a50efe3db054ebd2fb71baf463918
NamoSmith <namosmith2545@gmail.com>
2021-10-10 14:51:55 +0700

    Fix condition of vote method.

--- polls/views.py
+++ polls/views.py
@@ -1,43 +1,43 @@
+"""Programs for the view of polls."""
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-
 from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
+    """Index View Class."""
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
+        """Return the last five published questions (not including those set to be published in the future)."""
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
+    """Detail View Class."""
     model = Question
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+        """Excludes any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
+    """Result View class."""
     model = Question
-    template_name = 'polls/results.html'
+    template_name = 'polls/result.html'
 
 
 def vote(request, question_id):
+    """If there is no vote, redirect users to the page before and print an error message."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -47,10 +47,12 @@ def vote(request, question_id):
             'question': question,
             'error_message': "You didn't select a choice.",
         })
-    else:
+
+    if question.can_vote():
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    else:
+        return HttpResponseRedirect(reverse('polls:index'))
