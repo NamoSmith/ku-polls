70c4687702169c23af3bb3885e9bb24d8512c285
NamoSmith <namosmith2545@gmail.com>
2021-11-16 00:56:41 +0700

    :lock: Improve security.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7bswaqu9@^=ngmldbr*j27!2)ydw-vym#vxd%+$*)7(jo!)x#&'
+SECRET_KEY = env('SECRET_KEY', default='missing-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
